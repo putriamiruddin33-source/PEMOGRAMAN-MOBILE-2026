a1de6f612446dade16976a1157249d95df5c0282
putriamiruddin33-source <putriamiruddin33@gmail.com>
2026-04-08 20:07:23 +0800

    Update main.dart

--- Pertemuan 5/main.dart
+++ Pertemuan 5/main.dart
@@ -8,16 +8,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Aplikasi Kesehatan',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Aplikasi Sederhana'),
-          backgroundColor: Colors.green,
+          title: Text('Healthy App'),
+          backgroundColor: Colors.red,
         ),
         body: Center(
-          child: Text(
-            'Halo, Selamat Belajar Flutter!',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.favorite, size: 80, color: Colors.red),
+
+              SizedBox(height: 30),
+
+              // 🔽 JUDUL
+              Text(
+                'Selamat Datang di Aplikasi Kesehatan',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+
+              SizedBox(height: 15),
+
+              // 🔽 DESKRIPSI
+              Text(
+                'Aplikasi ini membantu Anda menjaga kesehatan dan gaya hidup sehat setiap hari.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+
+              SizedBox(height: 25),
+
+              Text(
+                'Tetap Sehat 💪',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[700],
+                ),
+              ),
+            ],
           ),
         ),
       ),
